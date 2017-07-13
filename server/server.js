0fbf63102e371d3e54136839bcc0f15feb3f9532
reid <rcarpe03@gmail.com>
2017-07-13 10:45:03 -0400

    add test cases for GET /todos/id

--- server/server.js
+++ server/server.js
@@ -21,13 +21,13 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// app.get('/todos', (req, res) => {
-//   Todo.find().then((todos) => {
-//     res.send({todos});
-//   }, (e) => {
-//     res.status(400).send(e);
-//   })
-// });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.get('/todos/:id', (req,res) => {
   var id = req.params.id;
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req,res) => {
   else if(ObjectID.isValid(id)) {
     Todo.findById(id).then((todo) => {
       if(todo){
-        res.status(200).send(id);
+        res.status(200).send({todo});
       }
       else {
         res.status(404).send();
